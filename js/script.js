dc145e2fab78227341fbf110d0be401f7e1c2f45
Nick Snitkin <nikita.snitkin@gmail.com>
2023-09-13 18:17:45 +0700

    project is ready

---
+++ js/script.js
@@ -0,0 +1,31 @@
+import tabs from './modules/tabs';
+import calc from './modules/calc';
+import cards from './modules/cards';
+import forms from './modules/forms';
+import modal from './modules/modal';
+import slider from './modules/slider';
+import timer from './modules/timer';
+import { openModal } from './modules/modal';
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    const modalTimerId = setTimeout(() => openModal('.modal,', modalTimerId), 300000);
+
+    tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
+    calc();
+    cards();
+    forms('form', modalTimerId);
+    modal('[data-modal]', '.modal', modalTimerId);
+    slider({
+        container:'.offer__slider', 
+        slide:'.offer__slide', 
+        nextArrow:'.offer__slider-next', 
+        prevArrow:'.offer__slider-prev', 
+        totalCounter:'#total', 
+        currentCounter:'#current', 
+        wrapper:'.offer__slider-wrapper', 
+        field:'.offer__slider-inner'
+    });
+    timer('.timer', '2023-10-02');
+
+});
